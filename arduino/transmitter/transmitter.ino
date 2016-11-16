27f5513040d0415fdeaa84bc29f7732a982a3f51
Nicolas <nico.encina2012@gmail.com>
2016-11-16 20:36:45 -0300

    Modificacion codigos transsmiter y receiver

--- arduino/transmitter/transmitter.ino
+++ arduino/transmitter/transmitter.ino
@@ -3,15 +3,18 @@
 
 /****************** User Config ***************************/
 /* Pines para CE y CSN respectivamente a los pines 3 y 4 */
-RF24 radio(3, 4);
-int LedPin1 = 2;        //LED se encenderá cuando exista comunicación
-int botonAnalog = 0;    //Se utiliza para medir un valor de voltaje a enviar
-/**********************************************************/
+
+#define STATE_LED 2             //LED se encenderá cuando no exista comunicación
+#define INTERRUPTOR_PIN 0       //Se utiliza para medir un valor de voltaje a enviar
+#define CE_PIN 3
+#define CSN_PIN 4
+
+RF24 radio(CE_PIN, CSN_PIN);
 byte addresses[][6] = {"1Node", "2Node"};
 
 void setup() {
   Serial.begin(115200);
-  pinMode(LedPin1, OUTPUT);
+  pinMode(STATE_LED, OUTPUT);
 
   radio.begin();
   radio.setPALevel(RF24_PA_LOW);
@@ -20,24 +23,25 @@ void setup() {
   radio.openReadingPipe(1, addresses[1]);
   
   // Start the radio listening for data
-  radio.startListening();
+  radio.stopListening();
 }
 
 void loop() {
-  radio.stopListening();                                    // First, stop listening so we can talk.
-  int16_t enviar = analogRead(botonAnalog);                            
+  //radio.stopListening();                                    // First, stop listening so we can talk.
+  int16_t mensaje = analogRead( INTERRUPTOR_PIN );                            
 
-  boolean intento=radio.write( &enviar, sizeof(int16_t) );
+  boolean enviar = radio.write( &mensaje, sizeof(int16_t) );
+  Serial.println(enviar);
 
-  if (intento){
-    digitalWrite(LedPin1, LOW);
-  } else{
-    digitalWrite(LedPin1, HIGH);
+  if (enviar){  
+    digitalWrite(STATE_LED, LOW);
   }
-
+  else {
+    digitalWrite(STATE_LED, HIGH);
+  }
   
   // Try again 1s later
-  delay(50);
+  delay(1000);
   
 } 
 
